179467a973637c1de9120f94d4bebe8019433dec
Alex Moraes de Oliveira <alxmoliveira@gmail.com>
2022-04-18 03:38:31 -0300

    Animate carousel

---
+++ js/components/main-carousel.js
@@ -0,0 +1,14 @@
+const items = document.querySelector('.main-carousel .items');
+const btnRight = document.querySelector('.main-carousel .arrow-button.-right');
+const btnLeft = document.querySelector('.main-carousel .arrow-button.-left');
+let pixels = 0
+
+btnRight.addEventListener('click', function() {
+    pixels += 70;
+    items.style = `transform: translateX(${pixels}px);`;
+});
+
+btnLeft.addEventListener('click', function() {
+    pixels -= 70;
+    items.style = `transform: translateX(${pixels}px);`;
+});
