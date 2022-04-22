edfb92411810ea860fb531d8c62076da7000ea0e
Alex Moraes de Oliveira <alxmoliveira@gmail.com>
2022-04-21 21:07:08 -0300

    Create carousel js

--- js/components/main-carousel.js
+++ js/components/main-carousel.js
@@ -1,14 +1,14 @@
 const items = document.querySelector('.main-carousel .items');
 const btnRight = document.querySelector('.main-carousel .arrow-button.-right');
 const btnLeft = document.querySelector('.main-carousel .arrow-button.-left');
-let pixels = 0
+let pixels = 0;
 
 btnRight.addEventListener('click', function() {
-    pixels += 70;
+    pixels += 130;
     items.style = `transform: translateX(${pixels}px);`;
 });
 
 btnLeft.addEventListener('click', function() {
-    pixels -= 70;
+    pixels -= 130;
     items.style = `transform: translateX(${pixels}px);`;
 });
